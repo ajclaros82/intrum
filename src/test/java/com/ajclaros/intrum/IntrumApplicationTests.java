a3d9a86437139951efee1bfae9917e899cdf46b1
adominguez <a.dominguez@proxymity.io>
2022-03-15 19:01:24 +0100

    Initial project setup

---
+++ src/test/java/com/ajclaros/intrum/IntrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajclaros.intrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
